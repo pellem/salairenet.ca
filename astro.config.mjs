14a317dd2f5fcc6cd2e94833ed39942530400334
freakconomics <101590104+freakconomics@users.noreply.github.com>
2025-05-03 22:59:10 -0400

    fix tailwind import

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,6 +2,7 @@ import { defineConfig } from 'astro/config';
 import tailwind from '@astrojs/tailwind';
 
 export default defineConfig({
+  integrations: [tailwind()],
   vite: {
     optimizeDeps: {
       disabled: false,
